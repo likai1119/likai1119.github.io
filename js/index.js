0a484783977b91a2692bf246e59e4de09919d779
石吉文 <shijw@eol.cn>
2024-06-15 21:27:46 +0800

    修改弹窗

--- js/index.js
+++ js/index.js
@@ -4,17 +4,19 @@
  * @Description: 
  */
 
-//打开页面时候弹出输入框输入密码，正确进入页面，点击取消继续弹出输入框
+let modal = document.getElementById("myModal");
+let btn = document.getElementById("submitBtn");
+let input = document.getElementById("nameInput");
+let img = document.querySelector('.bgimg')
 
-checkPassword()
-
-function checkPassword() {
-  var password = prompt("请输入密码：");
-  if (password === "123456") {
-    // 密码正确，进入页面
-    document.getElementById("content").style.display = "block";
+btn.onclick = function () {
+  var name = input.value;
+  if (name === 'wangzhe') {
+    modal.style.display = "none";
+    img.style.display = "block";
   } else {
-    // 密码错误，继续弹出输入框
-    checkPassword();
+    alert("访问密码错误");
+    modal.style.display = "block";
+    img.style.display = "none";
   }
 }
